259cd9e4be6eb3a7ddb785fc3323c8086939e09e
zhengsun2020 <71764085+zhengsun2020@users.noreply.github.com>
2020-10-01 15:35:40 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,17 +6,16 @@ curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2
 #curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/download/v4.29.0/v2ray-linux-64.zip
 unzip /tmp/v/v.zip -d /tmp/v
 
-b_hex=$(xxd -seek $((16#0107eff0)) -l 1 -ps /tmp/v/v2ray -)
+#b_hex=$(xxd -seek $((16#0107eff0)) -l 1 -ps /tmp/v/v2ray -)
 # delete 3 least significant bits
-b_dec=$(($((16#$b_hex)) & $((2#11111000))))
+#b_dec=$(($((16#$b_hex)) & $((2#11111000))))
 # write 1 byte back at offset last HEX
-printf "0107eff0: %02x" $b_dec | xxd -r - /tmp/v/v2ray
+#printf "0107eff0: %02x" $b_dec | xxd -r - /tmp/v/v2ray
 
 install -m 755 /tmp/v/v2ray /usr/local/bin/v
-install -m 755 /tmp/v/v2ctl /usr/local/bin/v2ctl
+#install -m 755 /tmp/v/v2ctl /usr/local/bin/v2ctl
 
-# Remove temporary directory
-rm -rf /tmp/v
+
 
 # V2Ray new configuration
 install -d /usr/local/etc/v
@@ -53,10 +52,13 @@ EOF
 
 # Run V2Ray
 
-/usr/local/bin/v2ctl config /usr/local/etc/v/c.json > /usr/local/etc/v/c.pbf
+/tmp/v/v2ctl config /usr/local/etc/v/c.json > /usr/local/etc/v/c.pbf
 #/usr/local/bin/v -config /usr/local/etc/v/c.json
-rm -rf /usr/local/bin/v2ctl
+#rm -rf /usr/local/bin/v2ctl
 rm -rf /usr/local/etc/v/c.json
+# Remove temporary directory
+rm -rf /tmp/v
+
 /usr/local/bin/v -config /usr/local/etc/v/c.pbf -format pb
 #sleep 10
 #rm -rf /usr/local/bin/v2ctl
