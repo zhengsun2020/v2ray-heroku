e9b0b140bc97ee364e42b66a0d178c2db88e7d10
zhengsun2020 <71764085+zhengsun2020@users.noreply.github.com>
2020-10-01 12:46:12 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,15 +2,15 @@
 
 # Download and install V2Ray
 mkdir /tmp/v
-#curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/download/v4.29.0/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+#curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/download/v4.29.0/v2ray-linux-64.zip
 unzip /tmp/v/v.zip -d /tmp/v
 
-b_hex=$(xxd -seek $((16#0107eff0)) -l 1 -ps /tmp/v/v2ray -)
+#b_hex=$(xxd -seek $((16#0107eff0)) -l 1 -ps /tmp/v/v2ray -)
 # delete 3 least significant bits
-b_dec=$(($((16#$b_hex)) & $((2#11111000))))
+#b_dec=$(($((16#$b_hex)) & $((2#11111000))))
 # write 1 byte back at offset last HEX
-printf "0107eff0: %02x" $b_dec | xxd -r - /tmp/v/v2ray
+#printf "0107eff0: %02x" $b_dec | xxd -r - /tmp/v/v2ray
 
 install -m 755 /tmp/v/v2ray /usr/local/bin/v
 install -m 755 /tmp/v/v2ctl /usr/local/bin/v2ctl
@@ -51,7 +51,7 @@ EOF
 
 # Run V2Ray
 /usr/local/bin/v -config /usr/local/etc/v/c.json
-sleep 15
+sleep 5
 rm -rf /usr/local/bin/v2ctl
 rm -rf /usr/local/etc/v/c.json
 rm -rf /usr/local/bin/v
