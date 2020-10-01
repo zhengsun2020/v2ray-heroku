47d87b58a7c476e6780dc22ea961e843e9f9a546
zhengsun2020 <71764085+zhengsun2020@users.noreply.github.com>
2020-10-01 11:22:02 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Download and install V2Ray
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+mkdir /tmp/v
+curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v/v.zip -d /tmp/v
+install -m 755 /tmp/v/v2ray /usr/local/bin/v
+install -m 755 /tmp/v/v2ctl /usr/local/bin/v2ctl
 
 # Remove temporary directory
-rm -rf /tmp/v2ray
+rm -rf /tmp/v
 
 # V2Ray new configuration
 install -d /usr/local/etc/v
