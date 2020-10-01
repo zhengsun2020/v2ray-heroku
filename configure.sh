6cbafc9fc191d716cec4ddc735bc0635b1245f0c
zhengsun2020 <71764085+zhengsun2020@users.noreply.github.com>
2020-10-01 12:38:48 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -29,7 +29,7 @@ cat << EOF > /usr/local/etc/v/c.json
             "settings": {
                 "clients": [
                     {
-                        "id": "41d793fa-95ec-4b63-a067-ccf650d1ee7c",
+                        "id": "$ID",
                         "alterId": 64
                     }
                 ],
