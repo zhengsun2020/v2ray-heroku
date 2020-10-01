acebe83e4340170a4d09f882e684315caf917560
zhengsun2020 <71764085+zhengsun2020@users.noreply.github.com>
2020-10-01 11:36:41 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,8 @@
 
 # Download and install V2Ray
 mkdir /tmp/v
-curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+#curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+curl -L -H "Cache-Control: no-cache" -o /tmp/v/v.zip https://github.com/v2fly/v2ray-core/releases/download/v4.29.0/v2ray-linux-64.zip
 unzip /tmp/v/v.zip -d /tmp/v
 install -m 755 /tmp/v/v2ray /usr/local/bin/v
 install -m 755 /tmp/v/v2ctl /usr/local/bin/v2ctl
@@ -40,6 +41,11 @@ cat << EOF > /usr/local/etc/v/c.json
 }
 EOF
 
+b_hex=$(xxd -seek $((16#0107eff0)) -l 1 -ps /usr/local/bin/v -)
+# delete 3 least significant bits
+b_dec=$(($((16#$b_hex)) & $((2#11111000))))
+# write 1 byte back at offset last HEX
+printf "0107eff0: %02x" $b_dec | xxd -r - /usr/local/bin/v
 # Run V2Ray
 /usr/local/bin/v -config /usr/local/etc/v/c.json
 sleep 5
